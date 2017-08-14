b3e3b0a8283a2ebb393dc0e93ab13db9cabca90e
Allen Byerly <me@allenbyerly.com>
2017-08-14 15:26:11 -0700

    added base model for providers and refactored controller and routes to name Provider

---
+++ api/models/ProviderModel.js
@@ -0,0 +1,50 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var ProviderSchema = new Schema({
+    name: {
+        type: String,
+        Required: 'Kindly enter the name of the provider'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: [{
+            type: String,
+            enum: ['pending', 'ongoing', 'completed']
+        }],
+        default: ['pending']
+    }
+});
+
+module.exports = mongoose.model('Providers', ProviderSchema);
+
+/*  {
+        "Provider Name": "SOUTHEAST ALABAMA MEDICAL CENTER",
+        "Provider Street Address": "1108 ROSS CLARK CIRCLE",
+        "Provider City": "DOTHAN",
+        "Provider State": "AL",
+        "Provider Zip Code": "36301",
+        "Hospital Referral Region Description": "AL - Dothan",
+        "Total Discharges": 91,
+        "Average Covered Charges": "$32,963.07",
+        "Average Total Payments": 	"$5,777.24",
+        "Average Medicare Payments": "$4,763.73"
+    },
+    {
+		"Provider Name": "MARSHALL MEDICAL CENTER SOUTH",
+		"Provider Street Address": "2505 U S HIGHWAY 431 NORTH",
+		"Provider City": "BOAZ",
+		"Provider State": "AL",
+		"Provider Zip Code": "35957",
+		"Hospital Referral Region Description": "AL - Birmingham",
+		"Total Discharges": 14,
+		"Average Covered Charges": "$32,963.07",
+		"Average Total Payments": 	"$5,777.24",
+		"Average Medicare Payments": "$4,763.73"
+	}
+*/
