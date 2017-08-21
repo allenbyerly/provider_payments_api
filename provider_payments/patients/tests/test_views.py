607731655d20d301ab6ddb4c22eeb226f1c3259f
Allen Byerly <me@allenbyerly.com>
2017-08-20 21:48:31 -0700

    added test for creating a new patient

--- provider_payments/patients/tests/test_views.py
+++ provider_payments/patients/tests/test_views.py
@@ -86,3 +86,62 @@ class GetSinglePatientTest(TestCase):
         response = client.get(
             reverse('get_delete_update_patient', kwargs={'pk': 30}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class CreateNewPatientTest(TestCase):
+    """ Test module for inserting a new patient """
+
+    def setUp(self):
+        timezone=pytz.timezone("America/Los_Angeles")
+
+        dob=datetime(2014, 1, 1, 0, 0)
+        dob=timezone.localize(dob)
+        self.valid_payload = {
+            'id': '111111111',
+            'first_name': 'Horton',
+            'date_of_birth': dob.isoformat(),
+            'social_security_number': '999-99-9999',
+            'primary_care_physician': 'Dr. Who'
+        }
+        dob=datetime(1969, 12, 25, 0, 0)
+        dob=timezone.localize(dob)
+        self.valid_payload = {
+            'id': '123456789',
+            'first_name': 'Hermes',
+            'date_of_birth': dob.isoformat(),
+            'social_security_number': '555-55-5555',
+            'primary_care_physician': 'Dr. Chrono'
+        }
+        dob=datetime(1955, 5, 5, 0, 0)
+        dob=timezone.localize(dob)
+        self.invalid_payload = {
+            'id':  '555555555',
+            'first_name': 'Dorothy',
+            'date_of_birth': dob.isoformat(),
+            'social_security_number': '123-45-6789',
+            'primary_care_physician': 'Dr. Oz'
+        }
+        dob=datetime(1955, 5, 5, 0, 0)
+        dob=timezone.localize(dob)
+        self.invalid_payload = {
+            'id':  '000000000',
+            'first_name': 'Alice',
+            'date_of_birth': dob.isoformat(),
+            'social_security_number': '222-22-2222',
+            'primary_care_physician': 'Dr. Dolittle'
+        }
+
+    def test_create_valid_patient(self):
+        response = client.post(
+            reverse('get_post_patients'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_patient(self):
+        response = client.post(
+            reverse('get_post_patients'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
