76ef25ecd95ccd9671e6d0ab5de955fe198c2e09
Allen Byerly <me@allenbyerly.com>
2017-08-20 21:32:34 -0700

    created test to get a single patient... previous tst only tested getting all patients

--- provider_payments/patients/tests/test_views.py
+++ provider_payments/patients/tests/test_views.py
@@ -46,3 +46,43 @@ class GetAllPatientsTest(TestCase):
         serializer = PatientSerializer(patients, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetSinglePatientTest(TestCase):
+    """ Test module for GET single patient API """
+
+    def setUp(self):
+        timezone=pytz.timezone("America/Los_Angeles")
+
+        dob=datetime(2014, 1, 1, 0, 0)
+        dob=timezone.localize(dob)
+        self.hermes = Patient.objects.create(
+            id= '123456789', first_name='Hermes', date_of_birth=dob, social_security_number='555-55-5555', primary_care_physician='Dr. Chrono')
+
+        dob=datetime(1969, 12, 25, 0, 0)
+        dob=timezone.localize(dob)
+        self.horton = Patient.objects.create(
+            id= '111111111', first_name='Horton', date_of_birth=dob, social_security_number='999-99-9999', primary_care_physician='Dr. Who')
+
+        dob=datetime(1955, 5, 5, 0, 0)
+        dob=timezone.localize(dob)
+        self.dorothy = Patient.objects.create(
+            id= '555555555', first_name='Dorothy', date_of_birth=dob, social_security_number='123-45-6789', primary_care_physician='Dr. Oz')
+
+        dob=datetime(1922, 2, 2, 0, 0)
+        dob=timezone.localize(dob)
+        self.alice = Patient.objects.create(
+            id= '000000000', first_name='Alice', date_of_birth=dob, social_security_number='222-22-2222', primary_care_physician='Dr. Dolittle')
+
+
+    def test_get_valid_single_patient(self):
+        response = client.get(
+            reverse('get_delete_update_patient', kwargs={'pk': self.horton.pk}))
+        patient = Patient.objects.get(pk=self.horton.pk)
+        serializer = PatientSerializer(patient)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_patient(self):
+        response = client.get(
+            reverse('get_delete_update_patient', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
