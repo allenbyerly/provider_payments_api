fd3fdf7b6f6a74562b4f9f499a3cde4e374354a0
Allen Byerly <me@allenbyerly.com>
2017-08-20 20:41:56 -0700

    added patient serializer

--- provider_payments/patients/views.py
+++ provider_payments/patients/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Patient
+from .serializers import PatientSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_patient(request, pk):
+    try:
+        patient = Patient.objects.get(pk=pk)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get details of a single patient
+    if request.method == 'GET':
+        return Response({})
+    # delete a single patient
+    elif request.method == 'DELETE':
+        return Response({})
+    # update details of a single patient
+    elif request.method == 'PUT':
+        return Response({})
+
+
+@api_view(['GET', 'POST'])
+def get_post_patients(request):
+    # get all patients
+    if request.method == 'GET':
+        return Response({})
+    # insert a new record for a patient
+    elif request.method == 'POST':
+        return Response({})
