5c926f1f9008334751072bc74349439ac0c56fe1
Allen Byerly <me@allenbyerly.com>
2017-08-20 22:56:27 -0700

    updated tests for testing delete of  a single patient

--- provider_payments/patients/tests/test_views.py
+++ provider_payments/patients/tests/test_views.py
@@ -192,3 +192,29 @@ class UpdateSinglePatientTest(TestCase):
             data=json.dumps(self.invalid_payload),
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class DeleteSinglePatientTest(TestCase):
+    """ Test module for deleting an existing patient record """
+
+    def setUp(self):
+        timezone=pytz.timezone("America/Los_Angeles")
+
+        dob=datetime(2014, 1, 1, 0, 0)
+        dob=timezone.localize(dob)
+        self.hermes = Patient.objects.create(
+            id= '123456789', first_name='Hermes', date_of_birth=dob, social_security_number='555-55-5555', primary_care_physician='Dr. Chrono')
+
+        dob=datetime(1955, 5, 5, 0, 0)
+        dob=timezone.localize(dob)
+        self.dorothy = Patient.objects.create(
+            id= '555555555', first_name='Dorothy', date_of_birth=dob, social_security_number='123-45-6789', primary_care_physician='Dr. Oz')
+
+    def test_valid_delete_patient(self):
+        response = client.delete(
+            reverse('get_delete_update_patient', kwargs={'pk': self.hermes.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_delete_patient(self):
+        response = client.delete(
+            reverse('get_delete_update_patient', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
