3902e9604e766ff1161c08a3c8a8dd1315eb8733
Allen Byerly <me@allenbyerly.com>
2017-09-04 15:51:58 -0700

    Cleanup and rename controller for queryadded testing framewrk and initial testing scripts

--- test/test.js
+++ test/test.js
@@ -31,88 +31,131 @@ chai.use(chaiHttp);
 
 
 describe('Blobs', function() {
-    it('should list ALL blobs on /blobs GET');
-    it('should list a SINGLE blob on /blob/<id> GET');
+    it('should list ALL providers on /providers GET');
+    it('should list a SINGLE provider on /provider/<id> GET');
     it('should add a SINGLE blob on /blobs POST');
     it('should update a SINGLE blob on /blob/<id> PUT');
     it('should delete a SINGLE blob on /blob/<id> DELETE');
 });
 
 
-describe('express rest api server', function(){
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//let mongoose = require("mongoose");
+//let Provider = require('../app/models/provider');
+
+//Require the dev-dependencies
+//let chai = require('chai');
+//let chaiHttp = require('chai-http');
+//let server = require('../server');
+//let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Provider', () => {
+        beforeEach((done) =;> { //Before each test we empty the database
+        Provider.remove({}, (err) => {
+            done();
+})
+}
+)
+})
+/*
+  * Test the /GET route
+  */
+describe('/GET providers', () => {
+    it('it should GET all the providers', (done) =;> {
+        chai.request(server)
+            .get('/providers')
+            .end((err, res) => {
+            res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(0);
+        done();
+})
+}
+)
+})
+describe('express rest api server', function() {
     var id;
 
-    it('post object', function(done){
+    it('post object', function (done) {
         superagent.post('http://localhost:3000/providers/')
-            .send({ name: 'John'
+            .send({
+                name: 'John'
                 , email: 'john@rpjs.co'
             })
-            .end(function(e,res){
+            .end(function (e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(res.body.length).to.eql(1);
                 expect(res.body[0]._id.length).to.eql(24);
                 id = res.body[0]._id;
-                done()
-            })
+                done();
+            });
     });
 
-    it('retrieves an object', function(done){
-        superagent.get('http://localhost:3000/providers/'+id)
-            .end(function(e, res){
+    it('retrieves an object', function (done) {
+        superagent.get('http://localhost:3000/providers/' + id)
+            .end(function (e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
                 expect(res.body._id.length).to.eql(24);
                 expect(res.body._id).to.eql(id);
-                done()
-            })
+                done();
+            });
     });
 
-    it('retrieves a collection', function(done){
+    it('retrieves a collection', function (done) {
         superagent.get('http://localhost:3000/providers/')
-            .end(function(e, res){
+            .end(function (e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(res.body.length).to.be.above(0);
-                expect(res.body.map(function (item){return item._id})).to.contain(id);
-                done()
-            })
+                expect(res.body.map(function (item) {
+                    return item._id
+                })).to.contain(id);
+                done();
+            });
     });
 
-    it('updates an object', function(done){
-        superagent.put('http://localhost:3000/providers/'+id)
-            .send({name: 'Peter'
-                , email: 'peter@yahoo.com'})
-            .end(function(e, res){
+    it('updates an object', function (done) {
+        superagent.put('http://localhost:3000/providers/' + id)
+            .send({
+                name: 'Peter'
+                , email: 'peter@yahoo.com'
+            })
+            .end(function (e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
                 expect(res.body.msg).to.eql('success');
-                done()
-            })
+                done();
+            });
     });
-    it('checks an updated object', function(done){
-        superagent.get('http://localhost:3000/providers/'+id)
-            .end(function(e, res){
+    it('checks an updated object', function (done) {
+        superagent.get('http://localhost:3000/providers/' + id)
+            .end(function (e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
                 expect(res.body._id.length).to.eql(24);
                 expect(res.body._id).to.eql(id);
                 expect(res.body.name).to.eql('Peter');
-                done()
-            })
+                done();
+            });
     });
 
-    it('removes an object', function(done){
-        superagent.del('http://localhost:3000/providers/'+id)
-            .end(function(e, res){
+    it('removes an object', function (done) {
+        superagent.del('http://localhost:3000/providers/' + id)
+            .end(function (e, res) {
                 // console.log(res.body)
                 expect(e).to.eql(null);
                 expect(typeof res.body).to.eql('object');
                 expect(res.body.msg).to.eql('success');
-                done()
-            })
-    })
+                done();
+            });
+    });
 });
