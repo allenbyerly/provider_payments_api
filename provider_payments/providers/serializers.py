3d7d66702b2da21d2de0d2ebc4264ac6462ab52b
Allen Byerly <me@allenbyerly.com>
2017-09-04 21:28:01 -0700

    clean up

--- provider_payments/providers/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Provider
-
-
-class ProviderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Provider
-        fields = ('id')
